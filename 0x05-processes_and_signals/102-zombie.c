c36c259c3e0e6d29c41b370ca828339964247aef
osei <williamsabodunrin@gmail.com>
2023-07-28 21:23:05 -0700

    zombie process

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * inifinite_while - runs an infinite while llop
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Entry poiint
+ * Return: 0
+ */
+int main(void)
+{
+	int pid, a = 0;
+
+	while (a < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			a++;
+		}
+		else
+			exit(0);
+	}
+
+	infinite_while();
+
+	return (0);
+}
