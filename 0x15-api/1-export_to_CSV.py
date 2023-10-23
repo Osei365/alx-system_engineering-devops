4e51f8a76e4ed3ba8767b2524f0608f881f18bff
Osei <williamsabodunrin@gmail.com>
2023-10-23 16:30:27 -0700

    csv file with REST API

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""using API to get todo of
+customers."""
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    filename = '{}.csv'.format(arg)
+    home = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get('{}users/{}'.format(home, arg))
+    uname = user.json().get('username')
+    todos = requests.get('{}users/{}/todos'.format(home, arg)).json()
+    with open(filename, 'w', encoding='utf-8') as csvfile:
+        csvwriter = csv.writer(csvfile, quotechar='"', quoting=csv.QUOTE_ALL)
+        for t in todos:
+            row = ['{}'.format(t.get('userId')),
+                   '{}'.format(uname),
+                   '{}'.format(t.get('completed')),
+                   '{}'.format(t.get('title'))]
+            csvwriter.writerow(row)
